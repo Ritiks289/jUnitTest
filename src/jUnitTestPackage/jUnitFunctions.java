e0afa4d3fa58b145f9685cfa3a3fc1d36df4bcf9
ritiksingh <ritiksingh@localhost>
2022-11-17 14:52:42 +0530

    Hello this is ritik

---
+++ src/jUnitTestPackage/jUnitFunctions.java
@@ -0,0 +1,14 @@
+package jUnitTestPackage;
+
+public class jUnitFunctions {
+	
+	public int addNumbers(int a, int b) {
+		int c = a+b;
+		return c;
+	}
+	public String addStrings(String a, String b) {
+		String c = a+b;
+		return c;
+	}
+
+}
